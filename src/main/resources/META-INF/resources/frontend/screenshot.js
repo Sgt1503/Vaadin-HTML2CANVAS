995421344c510972b463c888eaacaa03b88a0f55
Сергей Толстых <sergey.tolstyh@red-soft.ru>
2024-10-09 12:33:29 +0300

    Added second way to Screenshot. Realized with screen capture api
    (cherry picked from commit 6fec574f3670e6bd64d68838ba800030e756f624)

---
+++ src/main/resources/META-INF/resources/frontend/screenshot.js
@@ -0,0 +1,26 @@
+let capture;
+let blobValue;
+window.capture = async function() {
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    preferCurrentTab: true
+  });
+
+  const vid = document.createElement("video");
+
+  return new Promise((resolve, reject) => {
+    vid.addEventListener("loadedmetadata", function () {
+      const canvas = document.createElement("canvas"),
+            ctx = canvas.getContext("2d");
+      ctx.canvas.width = vid.videoWidth;
+      ctx.canvas.height = vid.videoHeight;
+      ctx.drawImage(vid, 0, 0, vid.videoWidth, vid.videoHeight);
+
+      stream.getVideoTracks()[0].stop();
+
+      resolve(canvas);
+    });
+
+    vid.srcObject = stream;
+    vid.play();
+  });
+}
